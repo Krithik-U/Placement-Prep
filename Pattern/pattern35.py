0e0ae7ecbaf6e24ea83208ffe6ef31014f1d42a1
Krithik-U <krithikumapathy@gmail.com>
2025-04-02 23:11:48 +0530

    pattern35 completed

---
+++ Pattern/pattern35.py
@@ -0,0 +1,10 @@
+row = int(input("Enter number of rows: "))
+count = row-1
+for i in range(row):
+    for j in range(row+i):
+        if j<count:
+            print(end=" ")
+        else:
+            print(i+1, end="")
+    count-=1
+    print()
