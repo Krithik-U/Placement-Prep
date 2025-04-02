11f61b39ddbae64d1d07fcde1046c7e303c4588b
Krithik-U <krithikumapathy@gmail.com>
2025-04-02 23:35:47 +0530

    pattern41 completed

---
+++ Pattern/pattern41.py
@@ -0,0 +1,12 @@
+row = int(input("Enter number of rows: "))
+count = row-1
+for i in range(row):
+    reset = 0
+    for j in range(row+i):
+        if j<count:
+            print(end=" ")
+        else:
+            print(chr(ord("A")+reset), end="")
+            reset+=1
+    count-=1
+    print()
