08b748188c0219ee7cf728d30035dbb4d18f3cb9
Krithik-U <krithikumapathy@gmail.com>
2025-04-03 12:22:20 +0530

    pattern47 completed

---
+++ Pattern/pattern47.py
@@ -0,0 +1,8 @@
+row = int(input("Enter number of rows: "))
+for i in range(row):
+    for j in range(2*row-i-1):
+        if j<i:
+            print(end=" ")
+        else:
+            print(end="*")
+    print()
