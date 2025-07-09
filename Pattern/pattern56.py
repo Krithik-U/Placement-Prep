9830fd154a691681bb7bf7a2b7dd014fe84e802d
Krithik-U <krithikumapathy@gmail.com>
2025-07-09 12:00:50 +0530

    pattern55 completed

--- Pattern/pattern56.py
+++ Pattern/pattern56.py
@@ -1,6 +1,6 @@
 row = int(input("Enter number of rows: "))
-for i in range(row+1):
-    for j in range(i):
+for i in range(row):
+    for j in range(i+1):
         print(row-i+j, end="")
     print()
 for i in range(row-1, 0, -1):
